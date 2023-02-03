0dcf32a591cf09af3f4e455fe7758730be15da51
toannvph05233 <johntoan98@gmail.com>
2023-02-03 11:01:35 +0700

    up crud ajax

--- home.js
+++ home.js
@@ -1,19 +1,95 @@
+let isCreate = true;
+function show() {
+    $.ajax({
+        type: "GET",
+        headers: {
+            'Accept': 'application/json'
+        },
+        url: "http://localhost:8080/blogs",
+        //xử lý khi thành công
+        success: function (blogs) {
+            console.log(blogs)
+            let str = '';
+            for (const blog of blogs) {
+                str += `<div class="col-sm-4">
+                        <h2>${blog.title}</h2>
+                        <img src="${blog.img}" width="300" height="200">
+                         <p>${blog.content}</p>
+                         <a type="button" class="btn btn-warning" onclick="showEdit(${blog.id})" data-toggle="modal" data-target="#myModal">Edit</a>
+                        <a type="button" class="btn btn-danger" >Delete</a>
+                        </div>`
+            }
 
-let blogs = [
-    {id:1,title:'Anh Bon', content: 'yeu em Hanh',img:'https://cdn.vntrip.vn/cam-nang/wp-content/uploads/2017/07/chup-anh-sen-ho-tay.jpg'},
-    {id:2,title:'Anh Bon 2', content: 'yeu em Hanh',img:'https://lapvui.com/wp-content/uploads/2022/04/tu-the-chup-anh-voi-hoa-sen-thumb.jpg'},
-    {id:3,title:'Anh Bon 3', content: 'yeu em Hanh',img:'https://sheis.vn/wp-content/uploads/2022/05/cac-tu-the-chup-anh-voi-hoa-sen-11.jpg'},
-]
+            document.getElementById("show").innerHTML = str;
 
-let str = '';
-for (const blog of blogs) {
-    str += `<div class="col-sm-4">
-            <h2>${blog.title}</h2>
-            <img src="${blog.img}" width="300" height="200">
-            <p>${blog.content}</p>
-            <a type="button" class="btn btn-warning" >Edit</a>
-            <a type="button" class="btn btn-danger" >Delete</a>
-        </div>`
+        },
+        error: function (err) {
+            console.log(err)
+        }
+    })
 }
 
-document.getElementById("show").innerHTML = str;
+show();
+
+function clearEdit() {
+    isCreate = true;
+    document.getElementById("id").value = 0;
+    $("#content").val("");
+    $("#title").val("");
+    $("#img").val("");
+}
+
+function create() {
+    let blog = {
+        "title": document.getElementById("title").value,
+        "content": $("#content").val(),
+        "img": $("#img").val(),
+        "category": {
+            "id": $("#idCategoty").val(),
+        }
+    }
+
+    if (!isCreate){
+        blog.id = $("#id").val();
+    }
+
+    $.ajax({
+        type: "Post",
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        url: "http://localhost:8080/blogs",
+        data: JSON.stringify(blog),
+        //xử lý khi thành công
+        success: function (data) {
+            alert("Thành công");
+            show();
+        },
+        error: function (err) {
+            console.log(err)
+        }
+    })
+}
+
+function showEdit(id) {
+    isCreate = false;
+    $.ajax({
+        type: "Get",
+        headers: {
+            'Accept': 'application/json'
+        },
+        url: "http://localhost:8080/blogs/" + id,
+        //xử lý khi thành công
+        success: function (blog) {
+            document.getElementById("id").value = blog.id;
+            $("#content").val(blog.content);
+            $("#title").val(blog.title);
+            $("#img").val(blog.img);
+            $("#idCategoty").val(blog.category.id);
+        },
+        error: function (err) {
+            console.log(err)
+        }
+    })
+}
